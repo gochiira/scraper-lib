83934b95c51f0180a89baa6b80500c58414ed572
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2020-05-26 04:39:12 +0900

    fetchLib: LineNotifyの実装

---
+++ LineNotifyApi/linenotify.py
@@ -0,0 +1,57 @@
+import requests
+
+
+class LineNotifyClient():
+    baseUrl = "https://notify-api.line.me/api/"
+    headers = {
+        "Authorization": "Bearer ",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    def __init__(self, token):
+        self.headers["Authorization"] += token
+
+    def sendNotify(
+        self,
+        message,
+        imageThumbnail=None,
+        imageFullsize=None,
+        notificationDisabled=False
+    ):
+        data = {
+            "message": message,
+            "imageThumbnail": imageThumbnail,
+            "imageFullsize": imageFullsize,
+            "notificationDisabled": notificationDisabled
+        }
+        resp = requests.post(
+            self.baseUrl + "notify",
+            headers=self.headers,
+            data=data
+        )
+        if resp.status_code in [401, 400]:
+            return Exception('Notify token error')
+        return resp.json()
+
+    def checkToken(self):
+        resp = requests.get(
+            self.baseUrl + "status",
+            headers=self.headers
+        )
+        if resp.status_code in [401, 400]:
+            return Exception('Notify token error')
+        return resp.json()
+
+    def revokeToken(self):
+        resp = requests.post(
+            self.baseUrl + "revoke",
+            headers=self.headers
+        )
+        if resp.status_code in [401]:
+            return Exception('Notify token error')
+        return resp.json()
+
+
+if __name__ == "__main__":
+    cl = LineNotifyClient("TOKEN")
+    print(cl.sendNotify('TEST', notificationDisabled=True))
